1f0c0d7e792105f56332385c984e220ec8256dbf
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-01-29 10:11:37 +0330

    first commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,7 +11,27 @@ var app = new Swiper(".app", {
     el: ".swiper-pagination",
   },
 });
-
+var blog = new Swiper(".blog", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 36,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 // aos
 AOS.init();
 
