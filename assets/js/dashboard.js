d46151319330d17ad1b2f3f6d75a0ed5fd632264
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-01-31 14:20:09 +0330

    first commit

---
+++ assets/js/dashboard.js
@@ -0,0 +1,11 @@
+// dashboard sidebar
+
+let hamIcon=document.getElementById('hamIcon');
+let closeIcon=document.getElementById('close-icon');
+
+hamIcon.addEventListener('click',function () {
+  document.querySelector('aside').classList.add('active');
+})
+closeIcon.addEventListener('click',function () {
+    document.querySelector('aside').classList.remove('active');
+  })
